a0a97eea6b54b497e6ec69b32be85e8998234130
Andrew Duthie <andrew@andrewduthie.com>
2020-09-10 20:47:42 -0400

    Update disable to support new feature flag toggle

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: Disable NUX
  * Plugin URI:  https://github.com/aduth/wp-disable-nux/
  * Description: Disables "New User Experience" guides.
- * Version:     1.0.0
+ * Version:     1.1.0
  * Author:      Andrew Duthie
  * Author URI:  https://andrewduthie.com
  * License:     GPLv2 or later
@@ -12,6 +12,9 @@
  */
 
 function dn_enqueue_scripts() {
-	wp_add_inline_script( 'wp-nux', 'wp.data.dispatch( "core/nux" ).disableTips();' );
+    wp_add_inline_script(
+        'wp-edit-post',
+        'wp.data.select( "core/edit-post" ).isFeatureActive( "welcomeGuide" ) && wp.data.dispatch( "core/edit-post" ).toggleFeature( "welcomeGuide" );'
+    );
 }
 add_action( 'admin_enqueue_scripts', 'dn_enqueue_scripts' );
