e15e853f43fb5b5830e15cfd3bc0f6fd93caf6f1
Marco Brughi <mbrughi@users.noreply.github.com>
2021-01-05 11:55:48 +0100

    Update index.php
    Add is_admin to check user access

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
 
 /**
- * Plugin Name: Disable NUX
- * Plugin URI:  https://github.com/aduth/wp-disable-nux/
- * Description: Disables "New User Experience" guides.
- * Version:     1.1.0
- * Author:      Andrew Duthie
+ * Plugin Name: DGWM
+ * Plugin URI:  https://github.com/mbrughi/wp-disable-nux/
+ * Description: Disables Gutenberg welcome modal.
+ * Version:     1.2.0
+ * Author:      Marco Brughi
+ * Author URI:  https://marcobrughi.com
+ * Fork of plugin: https://github.com/aduth/wp-disable-nux/       
+ * Andrew Duthie
  * Author URI:  https://andrewduthie.com
  * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-function dn_enqueue_scripts() {
-    wp_add_inline_script(
-        'wp-edit-post',
-        'wp.data.select( "core/edit-post" ).isFeatureActive( "welcomeGuide" ) && wp.data.dispatch( "core/edit-post" ).toggleFeature( "welcomeGuide" );'
-    );
+function mb_enqueue_admin_script() {
+	if ( is_admin() ) {
+		wp_add_inline_script(
+			'wp-edit-post',
+			'wp.data.select( "core/edit-post" ).isFeatureActive( "welcomeGuide" ) && wp.data.dispatch( "core/edit-post" ).toggleFeature( "welcomeGuide" );'
+		);
+	}
 }
-add_action( 'admin_enqueue_scripts', 'dn_enqueue_scripts' );
+add_action( 'admin_enqueue_scripts', 'mb_enqueue_admin_script' );
